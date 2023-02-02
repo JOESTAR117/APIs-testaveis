841f867769357b1338ef3dcab1f52f8b529aa163
Jackson Rios <juniorrios1231@gmail.com>
2023-02-02 18:38:11 -0300

    feat: test unit passed ✔

---
+++ test/unit/controllers/products_spec.js
@@ -0,0 +1,27 @@
+import ProductsController from '../../../src/controllers/products'
+import sinon from 'sinon'
+import { expect } from 'chai'
+
+describe('Controllers: Products', () => {
+    const defaultProduct = [
+        {
+            name: 'Default product',
+            description: 'product description',
+            price: 100,
+        },
+    ]
+
+    describe('get() products', () => {
+        it('should return a list of products', () => {
+            const request = {}
+            const response = {
+                send: sinon.spy(),
+            }
+            const productsController = new ProductsController()
+            productsController.get(request, response)
+
+            expect(response.send.called).to.be.true
+            expect(response.send.calledWith(defaultProduct)).to.be.true
+        })
+    })
+})
