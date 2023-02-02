7ea14d1ed1fe24375dd241987f1a5ac5adc57b89
Jackson Rios <juniorrios1231@gmail.com>
2023-02-02 17:10:15 -0300

    feat: test passed ✔

--- src/app.js
+++ src/app.js
@@ -4,5 +4,14 @@ const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/products', (req, res) =>
+    res.send([
+        {
+            name: 'Default product',
+            description: 'product description',
+            price: 100,
+        },
+    ])
+)
 
 export default app
--- test/integration/helper.js
+++ test/integration/helper.js
@@ -3,5 +3,5 @@ import chai from 'chai'
 import app from '../../src/app.js'
 
 global.app = app
-global.request = supertest
+global.request = supertest(app)
 global.expect = chai.expect
