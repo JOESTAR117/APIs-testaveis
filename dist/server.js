74f82a530f16e773a8f55befc4f089ae568aa056
Jackson Rios <juniorrios1231@gmail.com>
2023-02-19 15:10:20 +0000

    feat: dist removed✔

--- dist/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var _app = _interopRequireDefault(require("./app"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-const port = process.env.PORT || 3000;
-_app.default.listen(port, () => {
-  console.log(`App running port ${port}`);
-});
